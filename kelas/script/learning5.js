d566df7fb0a10ec1bade244848537ab057a02a73
Arif <dopymonster@gmail.com>
2026-04-27 11:26:11 +0800

    dialog and drop zone

---
+++ kelas/script/learning5.js
@@ -0,0 +1,51 @@
+let items = document.querySelectorAll(".item")
+let ITEM = document.querySelector(".ITEM")
+let dropZone = document.querySelector(".kosong")
+let dropArea = dropZone.getBoundingClientRect()
+let isDrag = false
+
+items.forEach(item => {
+	item.addEventListener("mousedown", e => {
+		ITEM.style.opacity = 1
+		ITEM.style.left = (e.pageX - ITEM.offsetWidth / 2) + "px"
+		ITEM.style.top = (e.pageY - ITEM.offsetHeight / 2) + "px"
+		dropZone.classList.add("higlight")
+		isDrag = true
+	})
+});
+
+document.addEventListener("mousemove", e => {
+	if (!isDrag) return
+	ITEM.style.left = (e.pageX - ITEM.offsetWidth / 2) + "px"
+	ITEM.style.top = (e.pageY - ITEM.offsetHeight / 2) + "px"
+
+	
+	let isHereDropZone =
+		e.clientX >= dropArea.left &&
+		e.clientX <= dropArea.right &&
+		e.clientY >= dropArea.top &&
+		e.clientY <= dropArea.bottom
+
+	if (isHereDropZone) dropZone.classList.add("oren")
+	else dropZone.classList.remove("oren")
+		
+
+})
+document.addEventListener("mouseup", e => {
+	ITEM.style.opacity = 0
+	dropZone.classList.remove("higlight")
+	isDrag = false
+
+
+
+	if(
+		e.clientX >= dropArea.left &&
+		e.clientX <= dropArea.right &&
+		e.clientY >= dropArea.top &&
+		e.clientY <= dropArea.bottom
+	){
+		console.log("drop");
+		
+	}
+
+})
